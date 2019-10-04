99571c7f6f80f541ad2b00b64bf8444298b1aa83
Gaston <gastonmuzas@gmail.com>
2019-10-04 14:24:45 -0300

    format

--- Utiles/TipoExp.java
+++ Utiles/TipoExp.java
@@ -10,49 +10,44 @@ package utiles;
  * @author Gaston
  */
 public class TipoExp {
-    
+
     private String tipoDato;
     private String operacion;
-    
-    private final String opCompatiblesInteger []= {"suma", "resta", "division", "multiplicacion", "asignacion"};
-    private String opCompatiblesBoolean [] = {"or","and","not"};
-    
-   
-    public TipoExp(String unTipoDato){
-    
-    	this.tipoDato=unTipoDato;
-    	
-    }	
-      
-    
-    public boolean verifCompatibilidadOperacion( String unaOperacion){
-    
-    boolean encontrado;	
-    if (this.tipoDato.equalsIgnoreCase("integer")){
-    	 encontrado=buscarOperacion(this.opCompatiblesInteger,unaOperacion);
+
+    private final String opCompatiblesInteger[] = {"suma", "resta", "division", "multiplicacion", "asignacion"};
+    private String opCompatiblesBoolean[] = {"or", "and", "not"};
+
+    public TipoExp(String unTipoDato) {
+
+        this.tipoDato = unTipoDato;
+
     }
-    else {
-    	encontrado=buscarOperacion(this.opCompatiblesBoolean,unaOperacion);
-    	
+
+    public boolean verifCompatibilidadOperacion(String unaOperacion) {
+
+        boolean encontrado;
+        if (this.tipoDato.equalsIgnoreCase("integer")) {
+            encontrado = buscarOperacion(this.opCompatiblesInteger, unaOperacion);
+        } else {
+            encontrado = buscarOperacion(this.opCompatiblesBoolean, unaOperacion);
+
+        }
+        return encontrado;
+
     }
-    return encontrado;
-        
-   }
-    
-    
-  private boolean buscarOperacion( String [] operadoresValidos,String unaOperacion){
-	  
-	  int i=0;
-      int l=operadoresValidos.length;
-      boolean salir=false;
-      while((i<l)&&(!salir)) {
-      	if (operadoresValidos[i].equalsIgnoreCase(unaOperacion))
-      		salir=true;
-      i++;
-      }
-	return salir;  
-  	}  
-    
-    
-} 
-    
+
+    private boolean buscarOperacion(String[] operadoresValidos, String unaOperacion) {
+
+        int i = 0;
+        int l = operadoresValidos.length;
+        boolean salir = false;
+        while ((i < l) && (!salir)) {
+            if (operadoresValidos[i].equalsIgnoreCase(unaOperacion)) {
+                salir = true;
+            }
+            i++;
+        }
+        return salir;
+    }
+
+}
